54239a5ea8c9fa862a4e8d2a68d525edae56e19a
ˉShãÐøwQuęeÑˉ <81854150+ShadowQu33n@users.noreply.github.com>
2021-08-03 13:22:10 +0000

    RuinedBot

---
+++ Patterns/EndCoop.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using LeagueBot.Patterns;
+
+namespace LeagueBot
+{
+    public class EndCoop : PatternScript
+    {
+        public override void Execute()
+        {
+            client.initialize(); // scripts are isolated. 
+
+            bot.log("Match ended.");
+
+            client.onGameEnd();
+
+            bot.wait(5 * 1000);
+
+            bot.waitProcessOpen(Constants.ClientProcessName);
+
+            bot.log("Closing client...");
+
+            client.closeClient();
+           
+            bot.wait(5 * 1000);
+
+            bot.log("Opening client..");
+
+            client.openClient();
+
+            bot.waitProcessOpen(Constants.ClientProcessName);
+
+            bot.executePattern("StartCoop");
+        }
+    }
+}
